5464707fa67438ccf7a98264fdfdbf725c922f73
millyanne93 <nmillyanne20@gmail.com>
2024-04-22 14:18:25 +0000

    change var to const

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false; // Change var to let
-  let task2 = true; // Change var to let
+  const task = false; 
+  const task2 = true; 
 
   if (trueOrFalse) {
-    let task = true; // Change var to let
-    let task2 = false; // Change var to let
+    const task = true; 
+    const task2 = false; 
   }
 
   return [task, task2];
