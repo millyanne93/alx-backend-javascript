681adc108223003b626690f43fd6884f6ddbd0b6
millyanne93 <nmillyanne20@gmail.com>
2024-04-25 19:01:19 +0000

    ES6 classes

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,28 +1,11 @@
-const cloneCarSymbol = Symbol();
-
-class Car {
+export default class Car {
   constructor(brand, motor, color) {
     this._brand = brand;
     this._motor = motor;
     this._color = color;
-    this[cloneCarSymbol] = this.cloneCar.bind(this);
-  }
-
-  get brand() {
-    return this._brand;
-  }
-
-  get motor() {
-    return this._motor;
-  }
-
-  get color() {
-    return this._color;
   }
 
   cloneCar() {
-    return new Car(this._brand, this._motor, this._color);
+    return new this.constructor(this._brand, this._motor, this._color);
   }
 }
-
-export default Car;
