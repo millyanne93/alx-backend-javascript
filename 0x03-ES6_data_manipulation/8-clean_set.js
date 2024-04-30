f7e1ce8f9fd5029b6b5c3fa5fef31bbb10337a77
millyanne93 <nmillyanne20@gmail.com>
2024-04-30 06:26:55 +0000

    data manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,9 @@
-/* eslint-disable array-callback-return */
-export default function cleanSet(set, startString) {
-  if (!startString || startString.length === 0) {
+export default function cleanSet(set, string) {
+  if (string === undefined || string.length === 0) {
     return '';
   }
-  return Array.from(set)
-    .filter((str) => str.startsWith(startString))
-    .map((str) => str.slice(startString.length))
+  return [...set]
+    .filter((str) => (str !== undefined ? str.startsWith(string) : ''))
+    .map((str) => (str !== undefined ? str.slice(string.length) : ''))
     .join('-');
 }
