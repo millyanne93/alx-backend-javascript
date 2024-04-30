8d5bcb54b33102a9ee13391f33536f8607322e7e
millyanne93 <nmillyanne20@gmail.com>
2024-04-30 06:05:20 +0000

    data manipulation

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,9 @@
+export default function cleanSet(set, startString) {
+  if (!startString || startString.length === 0) {
+    return '';
+  }
+  return Array.from(set)
+    .filter((str) => str.startsWith(startString))
+    .map((str) => str.slice(startString.length))
+    .join('-');
+}
