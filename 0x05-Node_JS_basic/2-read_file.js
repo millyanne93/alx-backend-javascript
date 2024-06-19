162614d8ec2814d109d6c715b583af65308a2c38
millyanne93 <nmillyanne20@gmail.com>
2024-06-19 06:37:22 +0000

    fix lint

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -5,17 +5,17 @@ function countStudents(path) {
     // Read data
     const data = fs.readFileSync(path, { encoding: 'utf-8' });
     // Split data and take only the list without header
-    const lines = data.split('\n').filter(line => line.trim() !== '');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
     // Give the header of data
     const header = lines[0].split(',');
     // Find firstname and field index
-    const idxFn = header.findIndex(ele => ele === 'firstname');
-    const idxFd = header.findIndex(ele => ele === 'field');
+    const idxFn = header.findIndex((ele) => ele === 'firstname');
+    const idxFd = header.findIndex((ele) => ele === 'field');
     // Declare two dictionaries for counting each field and storing list of students
     const fields = {};
     const students = {};
 
-    lines.slice(1).forEach(line => {
+    lines.slice(1).forEach((line) => {
       const list = line.split(',');
       if (!fields[list[idxFd]]) fields[list[idxFd]] = 0;
       fields[list[idxFd]] += 1;
