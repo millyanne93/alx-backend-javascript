0b007dc6cb7d0640103eb516521e00f4a5414711
millyanne93 <nmillyanne20@gmail.com>
2024-06-20 09:46:41 +0000

    5. Hooks

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,31 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./5-payment');
+
+const expect = chai.expect;
+
+describe('sendPaymentRequestToApi', () => {
+  let consoleSpy;
+
+  beforeEach(() => {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    consoleSpy.restore();
+  });
+
+  it('should log "The total is: 120" when called with 100 and 20', () => {
+    sendPaymentRequestToApi(100, 20);
+
+    expect(consoleSpy.calledOnce).to.be.true;
+    expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
+  });
+
+  it('should log "The total is: 20" when called with 10 and 10', () => {
+    sendPaymentRequestToApi(10, 10);
+
+    expect(consoleSpy.calledOnce).to.be.true;
+    expect(consoleSpy.calledWith('The total is: 20')).to.be.true;
+  });
+});
